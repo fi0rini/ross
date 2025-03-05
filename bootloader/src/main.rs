9089c535ed297b0919abf29b44b6929713dc85dd
nick fiorini <nick@astarcyber.com>
2025-03-04 23:11:43 -0500

    more cleanup for deps and bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,15 +1,31 @@
 #![no_std]
 #![no_main]
 
-
 #[cfg(not(test))]
 mod init;
 
-use xmodem::Xmodem;
 use pi;
 use pi::timer::spin_sleep;
 use pi::gpio::Gpio;
 use core::time::Duration;
+
+/// Start address of the binary to load and of the bootloader.
+const BINARY_START_ADDR: usize = 0x80000;
+const BOOTLOADER_START_ADDR: usize = 0x4000000;
+
+/// Pointer to where the loaded binary expects to be laoded.
+const BINARY_START: *mut u8 = BINARY_START_ADDR as *mut u8;
+
+/// Free space between the bootloader and the loaded binary's start address.
+const MAX_BINARY_SIZE: usize = BOOTLOADER_START_ADDR - BINARY_START_ADDR;
+
+/// Branches to the address `addr` unconditionally.
+unsafe fn jump_to(addr: *mut u8) -> ! {
+    asm!("br $0" : : "r"(addr as usize));
+    loop {
+        asm!("wfe" :::: "volatile")
+    }
+}
 
 #[unsafe(no_mangle)]
 fn kmain() -> ! {
